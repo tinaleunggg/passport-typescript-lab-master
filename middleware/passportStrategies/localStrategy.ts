e311800fe8f43014853e912601b9bb3f348bb351
tina.leunggg <tinaisworking@gmail.com>
2025-10-07 11:37:31 -0700

    first commit

---
+++ middleware/passportStrategies/localStrategy.ts
@@ -0,0 +1,46 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { getUserByEmailIdAndPassword, getUserById} from "../../controllers/userController";
+import { PassportStrategy } from '../../interfaces/index';
+
+const localStrategy = new LocalStrategy(
+  {
+    usernameField: "email",
+    passwordField: "password",
+  },
+  (email, password, done) => {
+    const user = getUserByEmailIdAndPassword(email, password);
+    return user
+      ? done(null, user)
+      : done(null, false, {
+          message: "Your login details are not valid. Please try again",
+        });
+  }
+);
+
+/*
+FIX ME (types) 😭
+*/
+// get called when user pass authentication
+passport.serializeUser(function (user: any, done: Function) {
+  done(null, user.id);
+});
+
+/*
+FIX ME (types) 😭
+*/
+passport.deserializeUser(function (id: number, done: Function) {
+  let user = getUserById(id);
+  if (user) {
+    done(null, user);
+  } else {
+    done({ message: "User not found" }, null);
+  }
+});
+
+const passportLocalStrategy: PassportStrategy = {
+  name: 'local',
+  strategy: localStrategy,
+};
+
+export default passportLocalStrategy;
